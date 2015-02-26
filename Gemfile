adac6c2075e4518c8202e3f8f2ee6c119b089e66
Bruno Casali <brunoocasali@gmail.com>
2015-02-26 17:13:42 -0300

    Rspec files added

--- Gemfile
+++ Gemfile
@@ -2,36 +2,30 @@ source 'https://rubygems.org'
 
 ruby '2.2.0'
 
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
-# Use postgresql as the database for Active Record
 gem 'pg'
-# Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
-# Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
-# Use jquery as the JavaScript library
+gem 'enumerate_it'
+gem 'devise'
+gem 'devise-i18n-views', '~> 0.3.3'
+gem 'simple_form', '~> 3.1.0'
+gem 'responders', '~> 2.0.2'
 gem 'jquery-rails'
-# Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
-# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
-
-# Use Unicorn as the app server
 gem 'unicorn'
 
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
-
 group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
-
-  # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
-
-  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+  gem 'rspec-rails', '~> 3.1.0'
+  gem 'factory_girl_rails'
+  gem 'forgery'
+  gem 'shoulda-matchers', '~> 2.7.0'
   gem 'spring'
+  gem 'simplecov', require: false
+  gem 'codeclimate-test-reporter', require: false
 end
 
 group :production do
---
+++ spec/rails_helper.rb
@@ -0,0 +1,86 @@
+ENV['RAILS_ENV'] ||= 'test'
+
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+else
+  require 'simplecov'
+
+  SimpleCov.configure do
+    coverage_dir File.join('.', 'tmp', 'code_analysis', 'coverage')
+  end
+
+  SimpleCov.start 'rails'
+end
+
+require 'spec_helper'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rspec/rails'
+require 'capybara/rspec'
+require 'capybara-screenshot/rspec'
+require 'capybara/poltergeist'
+require 'awesome_print'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+# Loading forgeries from a custom path
+Forgery.load_from! "#{Forgery.rails_root}/spec/support/forgery"
+
+# Checks for pending migrations before tests are run.
+# If you are not using ActiveRecord, you can remove this line.
+ActiveRecord::Migration.maintain_test_schema!
+
+# Include Warden::Test::Helpers is necessary to call the login_as method here inside spec helper file
+include Warden::Test::Helpers
+Warden.test_mode!
+
+Capybara.default_wait_time = 5
+
+Capybara.javascript_driver = :poltergeist
+
+# Keep up to the number of screenshots specified in the hash
+Capybara::Screenshot.prune_strategy = { keep: 20 }
+
+RSpec.configure do |config|
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) { DatabaseCleaner.clean_with :truncation }
+  config.before(:each) { DatabaseCleaner.strategy = :transaction }
+  config.before(:each, js: true) { DatabaseCleaner.strategy = :truncation }
+  config.before(:each) { DatabaseCleaner.start }
+  config.after(:each) { DatabaseCleaner.clean }
+
+  # Remove verbose methods from this hot girl
+  config.include FactoryGirl::Syntax::Methods
+
+  # Include Devise methods to the specs
+  config.include Devise::TestHelpers, type: :controller
+
+
+  config.before(:each) do
+    @current_user = FactoryGirl.create(:user)
+    login_as @current_user
+  end
+
+  # RSpec Rails can automatically mix in different behaviours to your tests
+  # based on their file location, for example enabling you to call `get` and
+  # `post` in specs under `spec/controllers`.
+  #
+  # You can disable this behaviour by removing the line below, and instead
+  # explicitly tag your specs with their type, e.g.:
+  #
+  #     RSpec.describe UsersController, :type => :controller do
+  #       # ...
+  #     end
+  #
+  # The different available types are documented in the features, such as in
+  # https://relishapp.com/rspec/rspec-rails/docs
+  config.infer_spec_type_from_file_location!
+
+  # Raise any error when dreprected function was found
+  config.raise_errors_for_deprecations!
+end
+
