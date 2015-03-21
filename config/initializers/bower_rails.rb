d6689eaace6a727c7b9df074fede6ec0e9e0448e
Bruno Casali <brunoocasali@gmail.com>
2015-03-21 00:41:42 -0300

    bower news

--- config/initializers/bower_rails.rb
+++ config/initializers/bower_rails.rb
@@ -6,11 +6,11 @@ BowerRails.configure do |bower_rails|
   # bower_rails.install_before_precompile = true
 
   # Invokes rake bower:resolve before precompilation. Defaults to false
-  # bower_rails.resolve_before_precompile = true
+  bower_rails.resolve_before_precompile = true
 
   # Invokes rake bower:clean before precompilation. Defaults to false
   # bower_rails.clean_before_precompile = true
-  
+
   # Invokes rake bower:install:deployment instead rake bower:install. Defaults to false
-  # bower_rails.use_bower_install_deployment = true
+  bower_rails.use_bower_install_deployment = true
 end
