d38c212c20f8444bbdc195dd07d8175215b93d3c
Bruno Casali <brunoocasali@gmail.com>
2015-04-17 10:51:02 -0300

    removed Dotenv from application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'dotenv'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv.load('.env')
 
 module InteractiveEvent
   class Application < Rails::Application
