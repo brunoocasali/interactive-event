e86f3c0d1b2c460ab51845226e3f57461c1d517c
Bruno Casali <brunoocasali@gmail.com>
2015-05-04 18:44:57 -0300

    simple solution to find new tweets :shipit:

--- app/services/search/twitter_service.rb
+++ app/services/search/twitter_service.rb
@@ -1,9 +1,30 @@
+# module Search
+#   class TwitterService
+#     # Isso é legal chamar dentro de um job
+#     def self.find_tweets_with(hash_tag)
+#       $twitter_client.search(hash_tag, result_type: :recent).take(5).each do |tweet|
+#         puts tweet.text
+#       end
+#     end
+#   end
+# end
+#
+
+
 module Search
   class TwitterService
     # Isso é legal chamar dentro de um job
     def self.find_tweets_with(hash_tag)
-      $twitter_client.search(hash_tag, result_type: :recent).take(10).each do |tweet|
+      # list = $twitter_client.search(hash_tag, result_type: :recent).take(500)
+      #
+      # loop do
+      #   puts " --some \n"
+      #   break if true
+      # end
+
+      $twitter_client.search(hash_tag, result_type: :recent).each do |tweet|
         puts tweet.text
+        break if tweet.id.to_s == '595337603660021760'
       end
     end
   end
