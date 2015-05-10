761ed0bba737f29464d0a9725e1063e4beb85423
Bruno Casali <brunoocasali@gmail.com>
2015-05-10 20:50:18 -0300

    removing facebook and instagram keys (fix errors)

--- app/enumerations/service_kind.rb
+++ app/enumerations/service_kind.rb
@@ -1,4 +1,4 @@
 class ServiceKind < EnumerateIt::Base
   # TODO: Uncomment and organize when create the methods at services/search/base.rb
-  associate_values twitter: 3, facebook: 1, instagram: 2
+  associate_values twitter: 3 #, facebook: 1, instagram: 2
 end
