4d5094c25588270c09121f55d7f7a87929472ee7
Bruno Casali <brunoocasali@gmail.com>
2015-05-12 18:41:07 -0300

    improved a lot the CRUD views for event

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,19 @@
 module EventsHelper
+  def show_as_icon(event)
+    kind = event.kind
+    icon = if kind.eql? EventKind::GRADUATION
+             'mdi-social-school circle'
+           elsif kind.eql? EventKind::FAIR
+             'mdi-action-today circle'
+           elsif kind.eql? EventKind::WEDDING
+             'mdi-action-favorite-outline circle'
+           elsif kind.eql? EventKind::SOCIAL_EVENT
+             'mdi-social-whatshot circle'
+           elsif kind.eql? EventKind::BIRTHDAY
+             'mdi-social-cake circle'
+           end
+
+    icon
+    # tag(:i, class: icon, style: 'font-size: 22px', title: event.kind_humanize)
+  end
 end
