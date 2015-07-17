a25ede3cfc2037996916871f687e7398a9960e61
Bruno Casali <brunoocasali@gmail.com>
2015-07-17 00:31:52 -0300

    added some error log to it

--- config/initializers/data_finder_task.rb
+++ config/initializers/data_finder_task.rb
@@ -2,8 +2,15 @@ require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler.singleton
 
-scheduler.every '2m' do
-  Event.will_happen.each do |event|
-    Search::Base.new(event).start_finder!
+scheduler.every '10s' do
+  begin
+    Event.will_happen.each do |event|
+      Search::Base.new(event).start_finder!
+    end
+  rescue => e
+    $stderr.puts '-' * 80
+    $stderr.puts e.message
+    $stderr.puts e.stacktrace
+    $stderr.puts '-' * 80
   end
 end
