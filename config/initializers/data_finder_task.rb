d2cf7147f5635470d843b9eb17ca7a967119036a
Bruno Casali <brunoocasali@gmail.com>
2015-07-16 23:02:28 -0300

    fixed rufus

--- config/initializers/data_finder_task.rb
+++ config/initializers/data_finder_task.rb
@@ -3,9 +3,7 @@ require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.singleton
 
 scheduler.every '2s' do
-  # Event.will_happen.each do |event|
-  #   Search::Base.new(event).start_finder!
-  # end
-
-  Rails.logger.info '----------------------------****---------------- WORKS'
+  Event.will_happen.each do |event|
+    Search::Base.new(event).start_finder!
+  end
 end
