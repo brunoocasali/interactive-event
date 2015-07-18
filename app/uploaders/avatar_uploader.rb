5bfc51413f200b1668e166e769d5b8cdef5b1b0d
Bruno Casali <brunoocasali@gmail.com>
2015-07-18 15:37:14 -0300

    updated in the specific uploaders

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,7 +1,7 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  storage :file
+  storage :dropbox
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
