c40122a316624d810cf5ed8ecd989240330eaf68
Bruno Casali <brunoocasali@gmail.com>
2015-07-18 16:01:23 -0300

    fixed will_happen scope and right user_access_token in dropbox

--- config/initializers/data_finder_task.rb
+++ config/initializers/data_finder_task.rb
@@ -2,17 +2,8 @@ require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler.singleton
 
-scheduler.every '2m' do
-  begin
-    Event.will_happen.each do |event|
-      Search::Base.new(event).start_finder!
-    end
-
-  rescue => e
-    Rails.logger.info "--------------------------*****-------- ERROS #{e}"
+scheduler.every '20s' do
+  Event.will_happen.each do |event|
+    Search::Base.new(event).start_finder!
   end
-end
-
-scheduler.every '5s' do
-  Rails.logger.info '--------------------------*****-------- WORKS'
 end
