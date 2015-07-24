2ba916b0498e62092c2c95befc96db09f8f3411e
Bruno Casali <brunoocasali@gmail.com>
2015-07-24 00:58:37 -0300

    added errors actions to 404/422/500

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'errors/file_not_found'
+
+  get 'errors/unprocessable'
+
+  get 'errors/internal_server_error'
+
   root 'welcome#index'
 
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' },
@@ -33,4 +39,8 @@ Rails.application.routes.draw do
   end
 
   resources :events
+
+  match '/404', to: 'errors#file_not_found', via: :all
+  match '/422', to: 'errors#unprocessable', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
 end
