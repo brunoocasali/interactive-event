c23f21934fb7056949d5547676bc939f18faf5a5
Bruno Casali <brunoocasali@gmail.com>
2015-07-30 20:55:45 -0300

    fixed email things

--- app/mailers/user_registration.rb
+++ app/mailers/user_registration.rb
@@ -1,7 +1,10 @@
 class UserRegistration < ApplicationMailer
   def first_instructions(user)
     @user = user
-    @user.reset_password_token = Devise.token_generator.generate(User, :reset_password_token)
+    @token = Devise.token_generator.generate(User, :reset_password_token)
+    @user.reset_password_token = @token.last
+    @user.reset_password_sent_at = Time.now
+    @user.save!
 
     mail(to: user.email, subject: I18n.t('mailers.user_registration.subject'),
          template_path: 'devise/mailer', template_name: 'first_instructions')
