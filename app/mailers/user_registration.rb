83d332c7bac0c8e228456df1d7096ff3152e4d3e
Bruno Casali <brunoocasali@gmail.com>
2015-07-30 18:59:25 -0300

    Update user_registration.rb

--- app/mailers/user_registration.rb
+++ app/mailers/user_registration.rb
@@ -2,7 +2,7 @@ class UserRegistration < ApplicationMailer
   def first_instructions(user)
     @user = user
     @user.lock_access!(send_instructions: false)
-    mail(to: user.email, subject: I18n.t('mailers.user_registration.subject'),
+    mail(to: user.unconfirmed_email, subject: I18n.t('mailers.user_registration.subject'),
          template_path: 'devise/mailer', template_name: 'first_instructions')
   end
 end
