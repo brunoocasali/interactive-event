25a021149c758dcb4dc238a4b77db6b2123c5684
Bruno Casali <brunoocasali@gmail.com>
2015-07-31 02:06:15 -0300

    try #22

--- app/mailers/user_registration.rb
+++ app/mailers/user_registration.rb
@@ -1,9 +1,9 @@
 class UserRegistration < ApplicationMailer
   def first_instructions(user, token)
     @user = user
-    @token = token
+    @token_raw = token
 
-    mail(to: user.email, subject: I18n.t('mailers.user_registration.subject'),
+    mail(to: @user.email, subject: I18n.t('mailers.user_registration.subject'),
          template_path: 'devise/mailer', template_name: 'first_instructions')
   end
 end
